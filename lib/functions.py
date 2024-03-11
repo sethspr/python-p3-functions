a15df6a90bfa0865a7e8ff3ac5538c425ab1f8ac
seth spring <seth.c.spring@gmail.com>
2024-03-11 15:28:12 -0400

    functions python complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,20 +1,41 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return(num1 + num2)
 
 def halve(number):
-    pass
+    return(number / 2)
+
+# def halve(number):
+#     number = 100
+#     return(f"{number:.2f} / 2")
+
+
+
+
+##### MODULE CODE-ALONG BELOW FOR PR
+
 
 def my_function(param):
     print("Running my_function")
     return param + 1
+
+my_function_return_value = my_function(1)
+print(my_function_return_value)
+
+def stylish_painter():
+    best_hairstyle = "Bob Ross"
+    return "Jean-Michel Basquiat"
+    return best_hairstyle
+    print(best_hairstyle)
+
+print(stylish_painter())
